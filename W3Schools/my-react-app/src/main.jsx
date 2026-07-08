d1c53702bd57e22bebbb469a12da31ee76df0ab3
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-08 15:04:31 +0530

    learning

---
+++ W3Schools/my-react-app/src/main.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client'
+const fruitlist = ['apple', 'banana', 'cherry'];
+
+function App() {
+  return (
+    <ul>
+      {fruitlist.map((fruit, index, array) =>
+        <>
+          <li key={fruit}>Name: {fruit}, Index: {index}, Array: {array}</li>
+        </>
+      )}
+    </ul>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
