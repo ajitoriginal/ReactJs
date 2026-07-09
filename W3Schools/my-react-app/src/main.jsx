5b7d2f2b7821e937ea7e3d6f9431beb8e51df7cb
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-09 15:42:49 +0530

    class components

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -1,24 +1,35 @@
 import { createRoot } from 'react-dom/client'
-const fruitlist = ['apple', 'banana', 'cherry'];
+import React from 'react';
 
-const kwtohp = (kw) => kw * 1.36
-
-function Car() {
-  const myObj = {
-    name: "Fiat",
-    model: "500",
-    color: "white"
-  };
-  return (
-    <>
-      <h2>My Car</h2>
-      <p>It is a {myObj.color} {myObj.name} & {myObj.model}</p>
-    </>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+    "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+        <div id="div1"></div>
+        <div id="div2"></div>
+      </div>
+    );
+  }
 }
 
 createRoot(document.getElementById('root')).render(
-  // <App />
-  <Car />
-  // myElement
-)
+  <Header />
+);
