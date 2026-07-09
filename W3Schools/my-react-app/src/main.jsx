113acf590671b875f22671936e5165691fd1db6b
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-09 15:58:36 +0530

    props

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -1,35 +1,44 @@
 import { createRoot } from 'react-dom/client'
-import React from 'react';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {favoritecolor: "red"};
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({favoritecolor: "yellow"})
-    }, 1000)
-  }
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    document.getElementById("div1").innerHTML =
-    "Before the update, the favorite was " + prevState.favoritecolor;
-  }
-  componentDidUpdate() {
-    document.getElementById("div2").innerHTML =
-    "The updated favorite is " + this.state.favoritecolor;
-  }
-  render() {
-    return (
-      <div>
-        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-        <div id="div1"></div>
-        <div id="div2"></div>
-      </div>
-    );
-  }
+function Son(props) {
+  return (
+    <div style={{background: 'lightgreen'}}>
+      <h2>Son {props.name}</h2>
+      <div>{props.children}</div>
+    </div>
+  );
+}
+
+function Daughter(props) {
+  const {brand, name} = props;
+  return (
+    <div style={{background: 'lightblue'}}>
+      <h2>Daughter {name}</h2>
+      <div>{props.children}</div>
+    </div>
+  );
+}
+
+function Parent() {
+  return (
+    <div>
+      <h1>My two Children</h1>
+      <Son name="Tony">
+        <p>
+          This was written in the Parent component,
+          but displayed as a part of the Son component
+        </p>
+      </Son>
+      <Daughter name="Kiara">
+        <p>
+          This was written in the Parent component,
+          but displayed as a part of the Daughter component
+        </p>
+      </Daughter>
+    </div>
+  );
 }
 
 createRoot(document.getElementById('root')).render(
-  <Header />
+  <Parent />
 );
