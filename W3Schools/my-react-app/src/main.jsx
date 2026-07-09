9cf91ba54faf4fe22e0c03ef4470e2d124ebbc20
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-09 11:54:50 +0530

    JSX Expressions

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -1,13 +1,18 @@
 import { createRoot } from 'react-dom/client'
 const fruitlist = ['apple', 'banana', 'cherry'];
 
+const kwtohp = (kw) => kw * 1.36
+
 function Car() {
-  const brand = "Ford";
-  const model = "Mustang";
+  const myObj = {
+    name: "Fiat",
+    model: "500",
+    color: "white"
+  };
   return (
     <>
       <h2>My Car</h2>
-      <p>It is a {brand} {model}</p>
+      <p>It is a {myObj.color} {myObj.name} & {myObj.model}</p>
     </>
   )
 }
