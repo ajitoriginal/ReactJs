028fc5b68891df37e41bdbdbef539e77a2da5b10
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-10 20:37:15 +0530

    React events

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -1,44 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 
-function Son(props) {
+function Football() {
+  const shoot = (a, e) => {
+    alert(a)
+    alert(e.type)
+    
+  }
   return (
-    <div style={{background: 'lightgreen'}}>
-      <h2>Son {props.name}</h2>
-      <div>{props.children}</div>
-    </div>
-  );
-}
-
-function Daughter(props) {
-  const {brand, name} = props;
-  return (
-    <div style={{background: 'lightblue'}}>
-      <h2>Daughter {name}</h2>
-      <div>{props.children}</div>
-    </div>
-  );
-}
-
-function Parent() {
-  return (
-    <div>
-      <h1>My two Children</h1>
-      <Son name="Tony">
-        <p>
-          This was written in the Parent component,
-          but displayed as a part of the Son component
-        </p>
-      </Son>
-      <Daughter name="Kiara">
-        <p>
-          This was written in the Parent component,
-          but displayed as a part of the Daughter component
-        </p>
-      </Daughter>
-    </div>
-  );
+    <button onClick={(event) => shoot('Goal', event)}>Take the shot!</button>
+  )
 }
 
 createRoot(document.getElementById('root')).render(
-  <Parent />
-);
+  <Football />
+)
