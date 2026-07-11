e8b53016d8e87a86e65471ed9636436da7262f38
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-11 14:25:03 +0530

    React Forms Submit

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -4,8 +4,12 @@ import {createRoot} from 'react-dom/client'
 function MyForm() {
   const [name, setName] = useState("")
   const handleChange = (e) => setName(e.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault(e)
+    alert(name)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Enter Your Name:
         <input
@@ -15,6 +19,7 @@ function MyForm() {
         />
       </label>
       <p>Current Value : {name}</p>
+      <input type="submit"/>
     </form>
   )
 }
