78b7107688c5502ab8934c2b9febe521d0049cf0
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-11 16:17:37 +0530

    React Radio buttons

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -2,83 +2,50 @@ import {useState} from 'react'
 import {createRoot} from 'react-dom/client'
 
 function MyForm() {
-  const [inputs, setInputs] = useState({
-    firstName: 'John',
-    lastName: 'Wick',
-    tomato: true,
-    onion: false
-  })
-  const handleChange = (e) => {
-    const target = e.target
-    const name = target.name
-    const value = target.type === "checkbox" ? target.checked : target.value
-    setInputs({...inputs, [name]: value})
-  }
+  const [selectedFruit, setSelectedFruit] = useState('banana');
 
-  const handleSubmit = (e) => {
-    
-    const flavour = inputs.tomato && inputs.onion ? 'Tomato & Onion' :
-                    inputs.tomato ? 'Tomato' :
-                    inputs.onion ? 'Onion' :
-                    'Plain'
-    alert(`Current Values: First Name: ${inputs.firstName}, Last Name: ${inputs.lastName}, Flavour: ${flavour}`)
-    e.preventDefault()
-  }
+  const handleChange = (event) => {
+    setSelectedFruit(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    alert(`Your favorite fruit is: ${selectedFruit}`);
+    event.preventDefault();
+  };
 
   return (
     <form onSubmit={handleSubmit}>
+      <p>Select your favorite fruit:</p>
       <label>
-        First Name:
-        <input
-          type="text"
-          name="firstName"
-          value={inputs.firstName}
-          onChange={handleChange}
-        />
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="apple" 
+          checked={selectedFruit === 'apple'} 
+          onChange={handleChange} /> Apple
       </label>
       <br />
-      <br />
       <label>
-        Last Name:
-        <input
-          type="text"
-          name="lastName"
-          value={inputs.lastName}
-          onChange={handleChange}
-        />
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="banana" 
+          checked={selectedFruit === 'banana'} 
+          onChange={handleChange} /> Banana
       </label>
       <br />
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="cherry" 
+          checked={selectedFruit === 'cherry'} 
+          onChange={handleChange} /> Cherry
+      </label>
       <br />
-      <p>Choose burger option</p>
-      <label>
-        Tomato:
-        <input
-          type="checkbox"
-          name="tomato"
-          checked={inputs.tomato}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Onion:
-        <input
-          type="checkbox"
-          name="onion"
-          checked={inputs.onion}
-          onChange={handleChange}
-        />
-      </label>
-      <p>
-        Current Values: First Name: {inputs.firstName}, Last Name: {inputs.lastName}, Flavour: {
-          inputs.tomato && inputs.onion ? 'Tomato & Onion' :
-          inputs.tomato ? 'Tomato' :
-          inputs.onion ? 'Onion' :
-          'Plain'
-        }
-      </p>
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
 createRoot(document.getElementById('root')).render(
