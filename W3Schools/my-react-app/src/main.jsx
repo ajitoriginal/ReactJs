8b8f52f743d19158e46f642f7c11006ec552797f
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-11 14:41:54 +0530

    React Selects

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -2,24 +2,25 @@ import {useState} from 'react'
 import {createRoot} from 'react-dom/client'
 
 function MyForm() {
-  const [myTxt, setMyTxt] = useState("")
-  const handleChange = (e) => setMyTxt(e.target.value)
-  const handleSubmit = (e) => {
-    e.preventDefault(e)
-    alert(myTxt)
-  }
+  const [myCar, setMyCar] = useState("Volvo")
+  const handleChange = (e) => setMyCar(e.target.value)
+  // const handleSubmit = (e) => {
+  //   e.preventDefault(e)
+  //   alert(myTxt)
+  // }
   return (
-    <form onSubmit={handleSubmit}>
-    {/* <form> */}
+    // <form onSubmit={handleSubmit}>
+    <form>
       <label>
-        Write here:
-        <textarea
-          value={myTxt}
-          onChange={(e) => handleChange(e)}
-        />
+        Chhose here:
+        <select value={myCar} onChange={handleChange}>
+          <option value="Ford">Ford</option>
+          <option value="Volvo">Volvo</option>
+          <option value="Flat">Flat</option>
+        </select>
       </label>
-      <p>Current Value : {myTxt}</p>
-      <input type="submit"/>
+      <p>Current Value : {myCar}</p>
+        {/* <input type="submit"/> */}
     </form>
   )
 }
