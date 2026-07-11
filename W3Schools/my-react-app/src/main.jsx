abd7292f6cb93cd0307d08fec5f04214b4344be8
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-11 14:22:17 +0530

    React Forms

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -1,17 +1,24 @@
+import {useState} from 'react'
 import {createRoot} from 'react-dom/client'
 
-function MyCars() {
-  const cars = ['Ford', 'BMW', 'Audi'];
+function MyForm() {
+  const [name, setName] = useState("")
+  const handleChange = (e) => setName(e.target.value)
   return (
-    <>
-      <h1>My Cars:</h1>
-      <ul>
-        {cars.map((car, index) => <li key={index}>I am a { car }</li>)}
-      </ul>
-    </>
-  );
+    <form>
+      <label>
+        Enter Your Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => handleChange(e)}
+        />
+      </label>
+      <p>Current Value : {name}</p>
+    </form>
+  )
 }
 
 createRoot(document.getElementById('root')).render(
-  <MyCars />
+  <MyForm />
 );
