b5baf31f96cb8ef528a4702a66f6d3e5c991c115
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-11 16:08:06 +0530

    React Checkbox

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -3,17 +3,26 @@ import {createRoot} from 'react-dom/client'
 
 function MyForm() {
   const [inputs, setInputs] = useState({
-    firstName: "Tony",
-    lastName: "Stark"
+    firstName: 'John',
+    lastName: 'Wick',
+    tomato: true,
+    onion: false
   })
   const handleChange = (e) => {
-    const {name, value} = e.target
+    const target = e.target
+    const name = target.name
+    const value = target.type === "checkbox" ? target.checked : target.value
     setInputs({...inputs, [name]: value})
   }
 
   const handleSubmit = (e) => {
+    
+    const flavour = inputs.tomato && inputs.onion ? 'Tomato & Onion' :
+                    inputs.tomato ? 'Tomato' :
+                    inputs.onion ? 'Onion' :
+                    'Plain'
+    alert(`Current Values: First Name: ${inputs.firstName}, Last Name: ${inputs.lastName}, Flavour: ${flavour}`)
     e.preventDefault()
-    alert(`Current Values: First Name: ${inputs.firstName}, Last Name: ${inputs.lastName}`)
   }
 
   return (
@@ -38,8 +47,34 @@ function MyForm() {
           onChange={handleChange}
         />
       </label>
+      <br />
+      <br />
+      <p>Choose burger option</p>
+      <label>
+        Tomato:
+        <input
+          type="checkbox"
+          name="tomato"
+          checked={inputs.tomato}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Onion:
+        <input
+          type="checkbox"
+          name="onion"
+          checked={inputs.onion}
+          onChange={handleChange}
+        />
+      </label>
       <p>
-        Current Values: First Name: {inputs.firstName}, Last Name: {inputs.lastName}
+        Current Values: First Name: {inputs.firstName}, Last Name: {inputs.lastName}, Flavour: {
+          inputs.tomato && inputs.onion ? 'Tomato & Onion' :
+          inputs.tomato ? 'Tomato' :
+          inputs.onion ? 'Onion' :
+          'Plain'
+        }
       </p>
       <button type="submit">Submit</button>
     </form>
