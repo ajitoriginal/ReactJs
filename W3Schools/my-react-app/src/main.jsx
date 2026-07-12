13449cd6d1443aee4833f3ebe39b18d50cf81d80
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2026-07-12 13:32:52 +0530

    CSS styling

--- W3Schools/my-react-app/src/main.jsx
+++ W3Schools/my-react-app/src/main.jsx
@@ -1,53 +1,24 @@
-import {useState} from 'react'
 import {createRoot} from 'react-dom/client'
+import {useState} from 'react'
+import './MyStylesheet.css'
 
-function MyForm() {
-  const [selectedFruit, setSelectedFruit] = useState('banana');
 
-  const handleChange = (event) => {
-    setSelectedFruit(event.target.value);
+const MyHeader = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Sans-Serif"
   };
-
-  const handleSubmit = (event) => {
-    alert(`Your favorite fruit is: ${selectedFruit}`);
-    event.preventDefault();
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <p>Select your favorite fruit:</p>
-      <label>
-        <input 
-          type="radio" 
-          name="fruit" 
-          value="apple" 
-          checked={selectedFruit === 'apple'} 
-          onChange={handleChange} /> Apple
-      </label>
-      <br />
-      <label>
-        <input 
-          type="radio" 
-          name="fruit" 
-          value="banana" 
-          checked={selectedFruit === 'banana'} 
-          onChange={handleChange} /> Banana
-      </label>
-      <br />
-      <label>
-        <input 
-          type="radio" 
-          name="fruit" 
-          value="cherry" 
-          checked={selectedFruit === 'cherry'} 
-          onChange={handleChange} /> Cherry
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <>
+      <h1>Hellow style!</h1>
+      <p>Add a little style!</p>
+    </>
+  )
 }
 
+
 createRoot(document.getElementById('root')).render(
-  <MyForm />
+  <MyHeader />
 );
